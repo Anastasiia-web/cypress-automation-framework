8e09d4f1e19282085ecb14004f42f7fda9ed4074
Anastasiia <anastasiyamarket@gmail.com>
2023-03-19 13:38:51 +0100

    elements iteration test is added

--- cypress/e2e/automationTestStore/iterate-over-elements.js
+++ cypress/e2e/automationTestStore/iterate-over-elements.js
@@ -1,16 +1,29 @@
 /// <reference types="cypress" />
-/// <reference types="cypress-xpath" />                
+// /// <reference types="cypress-xpath" />            // as we do not use it here
 
-describe('Iterate over elements', () => {
-    it('Log information of all Hair Care products', () => {
-        cy.visit("https://automationteststore.com/")
-        // using part of the url creating dynamic xpath selector by Ranorex Selocity tool
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click()
-    });
+describe("Iterate over elements", () => {
+  it.only("Log information of all Hair Care products", () => {
+    cy.visit("https://automationteststore.com/");
+    // using part of the url creating dynamic xpath selector by Ranorex Selocity tool
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+    cy.get(".fixed_wrapper .prdocutname")
+      // .should("have.length", 4)    // if we want to add the assertion about the length
+      .each(($li, index, $lis) => {
+        cy.log("Index " + index + " is for " + $li.text()); // Index 0 is for Seaweed Conditioner; Index 1 is for The Vert Shampoo
+      });
+  });
 
-    it('Add specific product to basket', () => {
-        cy.visit("https://automationteststore.com/")
-        // using part of the url creating dynamic xpath selector by Ranorex Selocity tool
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click()
+  it("Add specific product to basket", () => {
+    cy.visit("https://automationteststore.com/");
+    // using part of the url creating dynamic xpath selector by Ranorex Selocity tool
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+    cy.get(".fixed_wrapper .prdocutname").each(($li, index, $lis) => {
+      // index, $lis are not used here yet
+      if ($li.text().includes("Curls to straight Shampoo")) {
+        cy.wrap($li).click();
+        cy.get(".breadcrumb").should("contain", "Curls to straight Shampoo");
+        cy.get(".bgnone").should("contain", "Curls to straight Shampoo");
+      }
     });
+  });
 });
