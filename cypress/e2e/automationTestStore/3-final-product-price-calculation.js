9fe7eb73eb92a45b2cc2ad5277c578331193faec
Anastasiia <anastasiyamarket@gmail.com>
2023-03-24 00:39:10 +0100

    featured products calculation added

---
+++ cypress/e2e/automationTestStore/3-final-product-price-calculation.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe("All featured products names & price calculation", () => {
+  it("Featured products name", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("#featured .thumbnails.list-inline .prdocutname").as("productName");
+    cy.get("@productName").each(($li, index, $lis) => {
+      cy.log("Index " + index + " is for " + $li.text());
+    });
+  });
+
+  it.only("Featured products price sum", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get(
+      "#featured .thumbnails.list-inline .oneprice, #featured .thumbnails.list-inline .pricenew"
+    )
+      .invoke("text")
+      .as("productPrice");
+
+    cy.get("@productPrice").then(($price) => {
+      cy.log($price); // $29.50$19.00$28.00$38.50
+      var itemsSum = 0;
+      var itemPrice = $price.split("$");
+
+      var i;
+      for (i = 0; i < itemPrice.length; i++) {
+        cy.log(itemPrice[i]);
+        itemsSum += Number(itemPrice[i]);
+        cy.log("Items total: " + itemsSum);
+      }
+      expect(itemsSum).to.equal(115);
+    });
+  });
+});
