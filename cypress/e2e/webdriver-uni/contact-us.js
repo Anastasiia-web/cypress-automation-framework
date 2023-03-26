4823413cac589057c431a4ae9493bbd23728e997
Anastasiia <anastasiyamarket@gmail.com>
2023-03-26 11:58:48 +0200

    multiple tabs scenario added to "contact-us" test

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -2,26 +2,46 @@
 /// <reference types="Cypress" />                        // to be able to use cypress commands
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  it("Should be able to submit a successful submission via Contact Us form", () => {
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="First Name"]')
+      .type("Anastasiia");
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="Last Name"]')
+      .type("Ivashchenko");
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="Email Address"]')
+      .type("i@gmail.com");
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="Comments"]')
+      .type("Hi)");
+    cy.get('[value="SUBMIT"]').click();
+    cy.get("h1").should("contain", "Thank You for your Message!");
+  });
 
-    it("Should be able to submit a successful submission via Contact Us form", () => {
-        cy.visit(ContactUsFormUrl)
-        cy.get('[action="contact_us.php"]').find('[placeholder="First Name"]').type("Anastasiia")
-        cy.get('[action="contact_us.php"]').find('[placeholder="Last Name"]').type("Ivashchenko")
-        cy.get('[action="contact_us.php"]').find('[placeholder="Email Address"]').type("i@gmail.com")
-        cy.get('[action="contact_us.php"]').find('[placeholder="Comments"]').type("Hi)")
-        cy.get('[value="SUBMIT"]').click()
-        cy.get('h1').should('contain', "Thank You for your Message!")
-    })
-
-
-    it("Should not be able to submit a successful submission via Contact Us form as all fields are required", () => {
-        cy.visit(ContactUsFormUrl)
-        cy.get('[action="contact_us.php"]').find('[placeholder="First Name"]').type("Anastasiia")
-        cy.get('[action="contact_us.php"]').find('[placeholder="Last Name"]').type("Ivashchenko")
-        cy.get('[action="contact_us.php"]').find('[placeholder="Comments"]').type("Hi)")
-        cy.get('[value="SUBMIT"]').click()
-        cy.get('body').should('contain', "Error: all fields are required").and('contain', "Error: Invalid email address")
-        //or
-        cy.get('body').contains("Error: all fields are required Error: Invalid email address")
-    })
-})
+  it("Should not be able to submit a successful submission via Contact Us form as all fields are required", () => {
+    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="First Name"]')
+      .type("Anastasiia");
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="Last Name"]')
+      .type("Ivashchenko");
+    cy.get('[action="contact_us.php"]')
+      .find('[placeholder="Comments"]')
+      .type("Hi)");
+    cy.get('[value="SUBMIT"]').click();
+    cy.get("body")
+      .should("contain", "Error: all fields are required")
+      .and("contain", "Error: Invalid email address");
+    //or
+    cy.get("body").contains(
+      "Error: all fields are required Error: Invalid email address"
+    );
+  });
+});
